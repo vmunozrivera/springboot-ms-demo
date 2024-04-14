737e736f2ce2c0826a81870c54f405f855cd2743
vmunozrivera <35176705+vmunozrivera@users.noreply.github.com>
2024-04-14 15:09:46 -0400

    Initial commit

---
+++ companies-crud/src/test/java/net/versoft/companiescrud/CompaniesCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package net.versoft.companiescrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompaniesCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
